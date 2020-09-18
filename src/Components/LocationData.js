5367b4c070f4cdc114036dd7714079c6ffa2e558
NazmulKhan-2009 <nazmulustc09@gmail.com>
2020-09-18 22:27:32 +0600

    site uploaded

--- src/Components/LocationData.js
+++ src/Components/LocationData.js
@@ -1,14 +1,22 @@
+
+import sajek from "../Image/Sajek.png"
+import coxBazar from "../Image/Sreemongol.png"
+import sundorbon from '../Image/sundorbon.png'
+import room1 from "../Image/Rectangle 26.png"
+import room2 from "../Image/Rectangle 27.png"
+import room3 from "../Image/Rectangle 28.png"
+
 export const LocationData=[
   {
     id:1,
     location:"COX'S BAZAR",
     history:"Cox's Bazar is a city,fishing port, tourism centre and districy headquarters in southeastern Bangladesh.It is famous mostly for its long natural sandy beach,and it...",
     category:"city",
-    image:"image/Sreemongol.png",
+    image:coxBazar,
     about:"Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.",
     roomDetails:[
       {
-      image:'../image/Rectangle 26.png',
+      image:room1,
       title:"Light bright airy stylish apt & safe peaceful stay",
       p1:"4 guests   2 bedrooms   2 beds   2 baths",
       p2:"Wif Air conditioning Kitchen",
@@ -18,7 +26,7 @@ export const LocationData=[
       other:"$167 total"
       },
       {
-        image:'../image/Rectangle 27.png',
+        image:room2,
         title:"Light bright airy stylish apt & safe peaceful stay",
         p1:"4 guests   2 bedrooms   2 beds   2 baths",
         p2:"Wif Air conditioning Kitchen",
@@ -28,7 +36,7 @@ export const LocationData=[
         other:"$167 total"
         },
         {
-          image:'../image/Rectangle 28.png',
+          image:room3,
           title:"Light bright airy stylish apt & safe peaceful stay",
           p1:"4 guests   2 bedrooms   2 beds   2 baths",
           p2:"Wif Air conditioning Kitchen",
@@ -44,11 +52,11 @@ export const LocationData=[
     location:"SAJEK",
     history:"SAJEK is a city,fishing port, tourism centre and districy headquarters in southeastern Bangladesh.It is famous mostly for its long natural sandy beach,and it...",
     category:"city",
-    image:'../image/Sajek.png',
+    image:sajek,
     about:"Sajek Valley is a beautiful area. It is clean, fresh and calm. It is a union of Baghaichari Upazila under the Rangamati Hill Tracts is named after in the name of a river separating Bangladesh from India which flows into the Karnafuli.",
     roomDetails:[
       {
-      image:'../image/Rectangle 26.png',
+      image:room1,
       title:"Light bright airy stylish apt & safe peaceful stay",
       p1:"4 guests   2 bedrooms   2 beds   2 baths",
       p2:"Wif Air conditioning Kitchen",
@@ -58,7 +66,7 @@ export const LocationData=[
       other:"$167 total"
       },
       {
-        image:'../image/Rectangle 27.png',
+        image:room2,
         title:"Light bright airy stylish apt & safe peaceful stay",
         p1:"4 guests   2 bedrooms   2 beds   2 baths",
         p2:"Wif Air conditioning Kitchen",
@@ -68,7 +76,7 @@ export const LocationData=[
         other:"$167 total"
         },
         {
-          image:'../image/Rectangle 28.png',
+          image:'https://lh3.googleusercontent.com/hmIQSnaTdHas4sUalFZTwKUDdtPBB3gzLy2K4fw9q9wlDLzcUYqsJdl7bETm9MGItXGl_VMTwGd_6-ltNAezcBCtAhemO69JBosu3Q7-OtSCkRR3firrM4nGJHEvtur01meDG_Ly2w=w2400',
           title:"Light bright airy stylish apt & safe peaceful stay",
           p1:"4 guests   2 bedrooms   2 beds   2 baths",
           p2:"Wif Air conditioning Kitchen",
@@ -84,11 +92,11 @@ export const LocationData=[
     location:"Sunorbon",
     history:"SUNDORBON is a city,fishing port, tourism centre and districy headquarters in southeastern Bangladesh.It is famous mostly for its long natural sandy beach,and it...",
     category:"city",
-    image:"image/sundorbon.png",
+    image:sundorbon,
     about:"The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh. ",
     roomDetails:[
       {
-      image:'../image/Rectangle 26.png',
+        image:room1,
       title:"Light bright airy stylish apt & safe peaceful stay",
       p1:"4 guests   2 bedrooms   2 beds   2 baths",
       p2:"Wif Air conditioning Kitchen",
@@ -98,7 +106,7 @@ export const LocationData=[
       other:"$167 total"
       },
       {
-        image:'../image/Rectangle 27.png',
+        image:room2,
         title:"Light bright airy stylish apt & safe peaceful stay",
         p1:"4 guests   2 bedrooms   2 beds   2 baths",
         p2:"Wif Air conditioning Kitchen",
@@ -108,7 +116,7 @@ export const LocationData=[
         other:"$167 total"
         },
         {
-          image:'../image/Rectangle 28.png',
+          image:room3,
           title:"Light bright airy stylish apt & safe peaceful stay",
           p1:"4 guests   2 bedrooms   2 beds   2 baths",
           p2:"Wif Air conditioning Kitchen",
